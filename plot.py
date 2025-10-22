829f3579624acaca6c3df9da281dcd924c7480d2
stupidblackmonkey <greenherobrin1979@tutamail.com>
2025-10-22 20:41:14 +0300

    plot.py

--- plot.py
+++ plot.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(-15, 15)
-y = x**3 - 3*x**2 - x + 3
+x = np.linspace(-15, 15) #кол-во иксов = 30
+y = x**3 - 3*x**2 - x + 3 #формула функции
 
-plt.plot(x, y)
-plt.show()
+plt.plot(x, y) #делает график
+
+plt.show() #выводит график
